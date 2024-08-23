816539c6518b4c6c10f9a62a160416a5cd72802f
Days45 <castro.days45@gmail.com>
2024-08-23 19:43:32 -0300

    quinto commit

--- Cliente.cs
+++ Cliente.cs
@@ -3,5 +3,21 @@ using System;
 namespace EspacioCadeteria;
 public class Cliente
 {
-    
+    private string nombre;
+    private string direccion;
+    private string telefono;
+    private string DatosReferenciaDireccion;
+
+    public Cliente(string nombre, string direccion, string telefono, string datosReferenciaDireccion)
+    {
+        this.Nombre = nombre;
+        this.Direccion = direccion;
+        this.Telefono = telefono;
+        DatosReferenciaDireccion1 = datosReferenciaDireccion;
+    }
+
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Direccion { get => direccion; set => direccion = value; }
+    public string Telefono { get => telefono; set => telefono = value; }
+    public string DatosReferenciaDireccion1 { get => DatosReferenciaDireccion; set => DatosReferenciaDireccion = value; }
 }
