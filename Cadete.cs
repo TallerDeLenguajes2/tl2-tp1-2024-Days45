a37c046430070699731c5527da81e80d69426df3
Days45 <castro.days45@gmail.com>
2024-08-23 19:24:52 -0300

    tercer commit

--- Cadete.cs
+++ Cadete.cs
@@ -4,5 +4,16 @@ namespace EspacioCadeteria;
 
 public class Cadete
 {
+    private int id;
+    private string nombre;
+    private string direccion;
+    private List<Pedidos> listaPedidos;
 
+    public Cadete(int id, string nombre, string direccion, List<Pedidos> listaPedidos)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.listaPedidos = listaPedidos;
+    }
 }
--- Pedidos.cs
+++ Pedidos.cs
@@ -1,8 +1,13 @@
 using System;
 
 namespace EspacioCadeteria;
-
+public enum Estado{
+    
+}
 public class Pedidos
 {
-
+    private int Nro;
+    private string Obs;
+    private Cliente cliente;
+    private 
 }
